721aaa2731dd2b343242892ad7f7b4d514567b3c
Bhubanananda <redtrap2024@gmail.com>
2024-06-28 09:48:52 +0530

    test commit

---
+++ src/app/test-comp/test-comp.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test-comp',
+  standalone: true,
+  imports: [],
+  templateUrl: './test-comp.component.html',
+  styleUrl: './test-comp.component.scss'
+})
+export class TestCompComponent {
+
+}
