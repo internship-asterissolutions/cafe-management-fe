cc377c5a83d61a5cdde9d5c7fd5caa1148c169b9
amitdash1 <superiorcrew90@gmail.com>
2024-07-06 11:06:56 +0530

    b

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductComponent } from './product/product.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProductComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'cafe-management-fe';
---
+++ src/app/product/product.component.ts
@@ -0,0 +1,11 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  standalone: true,
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.scss']
+})
+export class ProductComponent  {
+
+}
